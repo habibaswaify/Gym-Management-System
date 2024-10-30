1122ba82d02772074fd7d60d0375c5367ba83652
Habiba <habibaswaify@hotmail.com>
2024-10-30 21:31:08 +0300

    Update Utils.java

--- Gym/src/backend/general/Utils.java
+++ Gym/src/backend/general/Utils.java
@@ -1,12 +1,13 @@
 package Gym.src.backend.general;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Utils {
 
     public Utils(){}
 
-    private String capitalize(String input){
+    public String capitalize(String input){
         String[] words  = input.split(" ");
         StringBuilder output = new StringBuilder(words[0].substring(0, 1).toUpperCase() + words[0].substring(1));
         String word;
@@ -17,41 +18,49 @@ public class Utils {
         return output.toString();
     }
 
-    private boolean validateName(String name){
+    public boolean validateName(String name){
         return name.matches("^[a-zA-Z][a-zA-Z '-]+$");
     }
 
-    private boolean validateDate(String date){
+    public boolean validateDate(String date){
         return date.matches("^\\d{4}-\\d{2}-\\d{2}$");
 
     }
-    private boolean validateMembership(String membership){
+    public boolean validateMembership(String membership){
         List<String> membershipTypes = List.of("monthly","yearly","premium");
         return membershipTypes.contains(membership.toLowerCase());
 
     }
-    private boolean validateStatus(String status){
+    public boolean validateStatus(String status){
         List<String> statusTypes = List.of("active","expired");
         return statusTypes.contains(status.toLowerCase());
 
     }
-    private boolean validateEmail(String email) {
+    public boolean validateEmail(String email) {
         return email.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9-]+[.][a-z]{2,6}$");
     }
 
-    private boolean validatePhoneNumber(String phoneNumber) {
+    public boolean validatePhoneNumber(String phoneNumber) {
         return phoneNumber.matches("^01[0-9]{9}$");
     }
 
-    private boolean validateTrainerID(String trainerID) {
+    public boolean validateTrainerID(String trainerID) {
         return trainerID.matches("^T\\d{3}$");
     }
 
-    private boolean validateMemberID(String memberID) {
+    public boolean validateMemberID(String memberID) {
         return memberID.matches("^M\\d{4}$");
     }
 
-    private boolean validateClassID(String classID) {
+    public boolean validateClassID(String classID) {
         return classID.matches("^C\\d{3}$");
+    }
+    public boolean recordExists(ArrayList<? extends Record> records,String key){
+        for(Record record:records){
+            if (record.getSearchKey().equalsIgnoreCase(key)){
+                return true;
+            }
+        }
+        return false;
     }
 }
