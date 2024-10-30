fbed776a48c5d0ceb182e91075ce03c911778993
Habiba <habibaswaify@hotmail.com>
2024-10-30 21:32:17 +0300

    Update trainerRoleWindow

--- Gym/src/frontend/trainerRoleWindow.java
+++ Gym/src/frontend/trainerRoleWindow.java
@@ -1,8 +1,13 @@
 package Gym.src.frontend;
 
-import javax.swing.*;
+import Gym.src.backend.trainer.TrainerRole;
 
-public class trainerRoleWindow {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+public class trainerRoleWindow extends JFrame{
     private JButton addMemberButton;
     private JButton viewRegistrationsButton;
     private JButton viewMembersButton;
@@ -10,5 +15,39 @@ public class trainerRoleWindow {
     private JButton viewClassesButton;
     private JButton registerMemberForClassButton;
     private JButton cancelRegistrationButton;
-    private JButton button1;
+    private JButton logoutButton;
+    private JPanel mainPanel;
+
+
+    public trainerRoleWindow() throws IOException {
+        TrainerRole trainerRole = new TrainerRole();
+        setTitle("Trainer Role");
+        setContentPane(mainPanel);
+        setVisible(true);
+        addMemberButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                AddMember addMember = new AddMember(trainerRole);
+                addMember.setVisible(true);
+            }
+        });
+        viewMembersButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ViewMembers viewMembers = new ViewMembers(trainerRole);
+                viewMembers.setVisible(true);
+
+            }
+        });
+        addClassButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+    }
+    public static void main(String[] args) throws IOException {
+        trainerRoleWindow trainerRoleWindow= new trainerRoleWindow();
+    }
 }
